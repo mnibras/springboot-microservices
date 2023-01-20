6ef9445395314327f4a8bf4b81dfd8732bc527c0
Nibras Sameeh <nibras.s@aeturnum.com>
2023-01-20 18:23:16 +0530

    added okta security to microservices

--- order-service/src/main/java/com/dailycodebuffer/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/dailycodebuffer/orderservice/controller/OrderController.java
@@ -7,6 +7,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,6 +19,7 @@ public class OrderController {
     private OrderService orderService;
 
     @PostMapping("/placeOrder")
+    @PreAuthorize("hasAuthority('Customer')")
     public ResponseEntity<Long> placeOrder(@RequestBody OrderRequest orderRequest) {
         long orderId = orderService.placeOrder(orderRequest);
         log.info("Order Id: {}", orderId);
@@ -25,6 +27,7 @@ public class OrderController {
     }
 
     @GetMapping("/{orderId}")
+    @PreAuthorize("hasAuthority('Admin') || hasAuthority('Customer')")
     public ResponseEntity<OrderResponse> getOrderDetails(@PathVariable long orderId) {
         OrderResponse orderResponse = orderService.getOrderDetails(orderId);
         return new ResponseEntity<>(orderResponse, HttpStatus.OK);
